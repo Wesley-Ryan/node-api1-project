1a216370530ea7cf580d23bacd66e5622b881b88
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-11-30 12:34:53 -0800

    created helpers to edit our database

--- index.js
+++ index.js
@@ -3,6 +3,58 @@ const shortID = require("shortid");
 
 const server = express();
 
+//DB
+let db = {
+  id: shortID.generate(),
+  name: "First User",
+  bio: "I was the first user to sign up on this server.",
+};
+
+//Helpers
+const User = {
+  getAllUsers() {
+    return db;
+  },
+  getUserByID(id) {
+    return db.find((user) => user.id === id);
+  },
+  postUser(user) {
+    const newUser = { id: shortID.generate(), name: user.name, bio: user.bio };
+    db.push(newUser);
+    return newUser;
+  },
+  editUser(id, changes) {
+    const user = this.getUserByID(id);
+    if (!user) {
+      return null;
+    }
+    const updatedUser = { id, ...changes };
+    db = db.map((user) => {
+      if (user.id === id) return updatedUser;
+      return user;
+    });
+    return updatedUser;
+  },
+  deleteUser(id) {
+    const user = this.getUserByID(id);
+    if (user) {
+      db = db.filter((user) => user.id != id);
+    }
+    return user;
+  },
+};
+//****End Points ****
+
+//get all users
+server.get("/users", () => {});
+//get user by id
+
+//post user
+
+// put/edit user
+
+// delete user
+
 server.use("*", (req, res) => {
   res.status(404).json({ message: "Error, Not Found" });
 });
